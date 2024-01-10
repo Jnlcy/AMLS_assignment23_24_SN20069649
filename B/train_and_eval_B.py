e9360d41f828693f4d21f1bba2e1802eba81dd5a
Jnlcy <jiayizhang01@gmail.com>
2024-01-10 18:39:43 +0000

    add requirement.txt

--- B/train_and_eval_B.py
+++ B/train_and_eval_B.py
@@ -10,10 +10,22 @@ folder = 'B/'
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras import layers
+from keras.layers import Dense, Conv2D,  MaxPool2D, Flatten, GlobalAveragePooling2D,  BatchNormalization, Layer, Add
+from keras.models import Sequential
+from keras.models import Model
 
 model_filename = "pathmnist_model.h5"
 data_flag = 'pathmnist'
+
+"""
+ResNet-18
+Reference:
+[1] K. He et al. Deep Residual Learning for Image Recognition. CVPR, 2016
+[2] K. He, X. Zhang, S. Ren, and J. Sun. Delving deep into rectifiers:
+Surpassing human-level performance on imagenet classification. In
+ICCV, 2015.
+"""
+
 
 def get_data(data_flag):
     input_root = 'Datasets/'
@@ -34,6 +46,132 @@ def get_data(data_flag):
 
     return X_train, y_train, X_test, y_test,X_val,y_val
 
+
+class ResnetBlock(Model):
+    """
+    A standard resnet block.
+    """
+
+    def __init__(self, channels: int, down_sample=False):
+        """
+        channels: same as number of convolution kernels
+        """
+        super().__init__()
+
+        self.__channels = channels
+        self.__down_sample = down_sample
+        self.__strides = [2, 1] if down_sample else [1, 1]
+
+        KERNEL_SIZE = (3, 3)
+        # use He initialization, instead of Xavier (a.k.a 'glorot_uniform' in Keras), as suggested in [2]
+        INIT_SCHEME = "he_normal"
+
+        self.conv_1 = Conv2D(self.__channels, strides=self.__strides[0],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME)
+        self.bn_1 = BatchNormalization()
+        self.conv_2 = Conv2D(self.__channels, strides=self.__strides[1],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME)
+        self.bn_2 = BatchNormalization()
+        self.merge = Add()
+
+        if self.__down_sample:
+            # perform down sampling using stride of 2, according to [1].
+            self.res_conv = Conv2D(
+                self.__channels, strides=2, kernel_size=(1, 1), kernel_initializer=INIT_SCHEME, padding="same")
+            self.res_bn = BatchNormalization()
+
+    def call(self, inputs):
+        res = inputs
+
+        x = self.conv_1(inputs)
+        x = self.bn_1(x)
+        x = tf.nn.relu(x)
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+
+        if self.__down_sample:
+            res = self.res_conv(res)
+            res = self.res_bn(res)
+
+        # if not perform down sample, then add a shortcut directly
+        x = self.merge([x, res])
+        out = tf.nn.relu(x)
+        return out
+
+
+class ResNet18(Model):
+
+    def __init__(self, num_classes, **kwargs):
+        """
+            num_classes: number of classes in specific classification task.
+        """
+        super().__init__(**kwargs)
+        self.conv_1 = Conv2D(64, (7, 7), strides=2,
+                             padding="same", kernel_initializer="he_normal")
+        self.init_bn = BatchNormalization()
+        self.pool_2 = MaxPool2D(pool_size=(2, 2), strides=2, padding="same")
+        self.res_1_1 = ResnetBlock(64)
+        self.res_1_2 = ResnetBlock(64)
+        self.res_2_1 = ResnetBlock(128, down_sample=True)
+        self.res_2_2 = ResnetBlock(128)
+        self.res_3_1 = ResnetBlock(256, down_sample=True)
+        self.res_3_2 = ResnetBlock(256)
+        self.res_4_1 = ResnetBlock(512, down_sample=True)
+        self.res_4_2 = ResnetBlock(512)
+        self.avg_pool = GlobalAveragePooling2D()
+        self.flat = Flatten()
+        self.fc = Dense(num_classes, activation="softmax")
+
+    def call(self, inputs):
+        out = self.conv_1(inputs)
+        out = self.init_bn(out)
+        out = tf.nn.relu(out)
+        out = self.pool_2(out)
+        for res_block in [self.res_1_1, self.res_1_2, self.res_2_1, self.res_2_2, self.res_3_1, self.res_3_2, self.res_4_1, self.res_4_2]:
+            out = res_block(out)
+        out = self.avg_pool(out)
+        out = self.flat(out)
+        out = self.fc(out)
+        return out
+
+
+train_ds=tf.data.Dataset.from_tensor_slices((train_images,train_labels))
+test_ds=tf.data.Dataset.from_tensor_slices((test_images,test_labels))
+
+#visualize some examples
+plt.figure(figsize=(30,30))
+for i,(image,label) in enumerate(train_ds.take(20)):
+    #print(label)
+    ax=plt.subplot(5,5,i+1)
+    plt.imshow(image)
+    plt.title(CLASS_NAMES[label.numpy()[0]])
+    plt.axis('off')
+
+def process_image(image,label):
+    image=tf.image.per_image_standardization(image)
+    image=tf.image.resize(image,(64,64))
+    
+    return image,label
+
+train_ds_size=tf.data.experimental.cardinality(train_ds).numpy()
+test_ds_size=tf.data.experimental.cardinality(test_ds).numpy()
+print('Train size:',train_ds_size)
+print('Test size:',test_ds_size)
+
+
+train_ds=(train_ds
+          .map(process_image)
+          .shuffle(buffer_size=train_ds_size)
+          .batch(batch_size=32,drop_remainder=True)
+         )
+test_ds=(test_ds
+          .map(process_image)
+          .shuffle(buffer_size=test_ds_size)
+          .batch(batch_size=32,drop_remainder=True)
+         )
+
+
+
 # Load and preprocess your data
 training_images, training_labels, test_images, test_labels,val_images, val_labels = get_data(data_flag)
 
@@ -42,7 +180,7 @@ training_labels = tf.keras.utils.to_categorical(training_labels, num_classes=9)
 test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=9)
 val_labels = tf.keras.utils.to_categorical(val_labels, num_classes=9)
 
-# Define the MLP model using Keras Sequential API
+'''# Define the MLP model using Keras Sequential API
 model = tf.keras.Sequential(name = 'Firstmodel')
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 3)))
@@ -50,28 +188,27 @@ model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Dropout(0.25, name='Dropout2'))
 
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
-
-model.add(layers.Dense(128, activation='relu' ))
 model.add(layers.Dense(64, activation='relu' ))
-model.add(layers.Dropout(0.25, name='Dropout3'))
 
+model.add(layers.Dropout(0.5, name='Dropout3'))
 model.add(layers.Dense(9, activation='softmax'))
-model.summary()
+model.summary()'''
 
-#learning rate decay
+model = ResNet18(9)
+model.build(input_shape = (None,28,28,3))
 
-#optimizer
-opt = tf.keras.optimizers.Adam()
 
 # Compile the model
-model.compile(optimizer=opt,
+model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
+model.summary()
+
+
+
 
 # Early stopping callback
 early_stopping = tf.keras.callbacks.EarlyStopping(
@@ -95,12 +232,14 @@ datagen.fit(training_images)
 
 
 
-epochs = 30
+
 # Train the model
-history = model.fit(datagen.flow(training_images, training_labels, batch_size=32),
-          epochs=epochs,
+history = model.fit(datagen.flow(training_images, training_labels, batch_size=256),
+          epochs=50,
+          batch_size = 32,
           validation_data=(val_images, val_labels),
           callbacks=[early_stopping])
+
 # Evaluate the model
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
