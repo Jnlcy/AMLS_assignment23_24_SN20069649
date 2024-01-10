f72beb62c1056dda4371eeab8ed166d87b4960ea
Jnlcy <jiayizhang01@gmail.com>
2024-01-10 14:25:40 +0000

    change to model structure

--- B/train_and_eval_B.py
+++ B/train_and_eval_B.py
@@ -36,7 +36,7 @@ def get_data(data_flag):
 
 # Load and preprocess your data
 training_images, training_labels, test_images, test_labels,val_images, val_labels = get_data(data_flag)
-
+shuffle_indices = np.random.permutation(len(preprocessed_images))
 # Convert labels to one-hot encoding
 training_labels = tf.keras.utils.to_categorical(training_labels, num_classes=9)
 test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=9)
@@ -47,35 +47,43 @@ model = tf.keras.Sequential(name = 'Firstmodel')
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Dropout(.2, name='Dropout1'))
+
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
-model.add(layers.Dropout(.2, name='Dropout2'))
-model.add(layers.Dense(128, activation='relu',
-                       kernel_regularizer=tf.keras.regularizers.L1(0.01)))
-model.add(layers.Dense(9, activation='softmax',kernel_regularizer=tf.keras.regularizers.L1(0.01)))
+model.add(layers.Dense(128, activation='relu' ))
+
+model.add(layers.Dropout(0.25, name='Dropout3'))
+model.add(layers.Dense(9, activation='softmax'))
 model.summary()
+
+#learning rate decay
+
+#optimizer
+opt = tf.keras.optimizers.Adam()
+
 # Compile the model
-model.compile(optimizer='adam',
+model.compile(optimizer=opt,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # Early stopping callback
 early_stopping = tf.keras.callbacks.EarlyStopping(
     monitor='val_loss',  # Monitor the validation loss
-    patience=5,         # Number of epochs with no improvement after which training will be stopped
+    patience=3,         # Number of epochs with no improvement after which training will be stopped
     restore_best_weights=True  # Restores model weights from the epoch with the best value of the monitored metric
 )
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True)
 
 #data augmentation
 datagen = ImageDataGenerator(
     rotation_range=10,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
     shear_range=0.2,
-    zoom_range=0.1,
+    zoom_range=0.2,
     horizontal_flip=False,
     fill_mode='nearest')
     # Fit the generator to your data
@@ -83,7 +91,7 @@ datagen.fit(training_images)
 
 
 
-epochs = 50
+epochs = 30
 # Train the model
 history = model.fit(datagen.flow(training_images, training_labels, batch_size=32),
           epochs=epochs,
@@ -93,6 +101,8 @@ history = model.fit(datagen.flow(training_images, training_labels, batch_size=32
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
 
+# Save the model
+model.save(os.path.join(folder,model_filename))
 
 #plot history
 fig, (ax1, ax2) = plt.subplots(1,2, figsize=(16,6))
@@ -112,6 +122,5 @@ ax2.legend()
 ax2.set_xticks(range(1,len(history.history['loss'])+1,3))
 ax2.set_title('Accuracy', fontsize=title_fontsize)
 ax2.set_xlabel('Epoch', fontsize=axis_fontsize) 
+plt.show()
 
-# Save the model
-model.save(os.path.join(folder,model_filename))
